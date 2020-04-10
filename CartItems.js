2c19c81497f3781c49a3932a3135550b880c7adf
ADP <60855763+ayushsoni6476@users.noreply.github.com>
2020-04-10 17:30:08 +0530

    Add files via upload

---
+++ CartItems.js
@@ -0,0 +1,51 @@
+import React from "react";
+export default function CartItem({Item,value}){
+    const {id,title,img,price,total,count}=Item;
+    const {increment,decrement,removeItem}=value;
+    return(
+        <div className="row my-2 text-capitalize text-centre">
+
+        <div className="col-10 mx-auto col-lg-2">
+          <img
+          src={img}
+          style={{width:"5rem",height:"5rem"}}
+          className="img-fluid"
+          alt="product"
+          />
+        </div>
+        <div className="col=10 mx-auto col-lg-2">
+        <span className="d-lg-none">Product:</span>
+        {title}
+    </div>
+    <div className="col=10 mx-auto col-lg-2">
+        <span className="d-lg-none">Price:</span>
+        {price}
+    </div>
+    <div className="col-10 mx-auto col-lg-2my-2 my-lg-0">
+        <div className="d-flex justify-content-centre">
+            <div>
+                <span className="btn btn-black mx-1" onClick={()=>
+                    decrement(id)}>
+                        -
+                        </span>
+                        <span className="btn btn-black mx-1">{count}</span>
+                        <span className="btn btn-black mx-1" onclick={()=>
+                        increment(id)}>
+                            +
+                            </span>       
+        </div>
+        </div>
+    </div>
+    {/* */}
+    <div className="col-10 mx-auto col-lg-2">
+ <div className="cart-icon" onClick={()=>removeItem(id)}>
+     <i className="fas fa-trash"></i>
+
+ </div>
+ </div>
+ <div className="col-10 mx-auto col-lg-2">
+                        <strong>item total: Rs. {total}</strong>
+ </div>
+ </div>
+    );
+}
